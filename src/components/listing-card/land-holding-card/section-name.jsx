c6a8a113dc06ab89b777bd573d7030d398c7a697
Madison Saylor <maddie0264@gmail.com>
2024-08-17 10:05:17 -0400

    ensure section name validity

--- src/components/listing-card/land-holding-card/section-name.jsx
+++ src/components/listing-card/land-holding-card/section-name.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   FormControl, FormLabel, Input,
   FormErrorMessage, FormHelperText,
@@ -10,12 +10,29 @@ import {
   sectionNameHelper, townshipError, rangeError,
   sectionError,
 } from '../../../utils/text-utils';
-import { isRangeInvalid, isTownshipInvalid } from '../../../utils/listing-utils';
 
 function SectionName({ data, setData }) {
+  const sectionNameInput = `${data.section}-${data.township}-${data.range}`;
+  const [townshipInvalidity, setTownshipInvalidity] = useState(true);
+  const [rangeInvalidity, setRangeInvalidity] = useState(true);
+
+  // Check that township ends in a valid character
   useEffect(() => {
-    setData({ ...data, sectionName: `${data.section}-${data.township}-${data.range}` });
-  }, [data, setData]);
+    if (data.township[3] === 'N' || data.township[3] === 'S') {
+      setTownshipInvalidity(false);
+    } else {
+      setTownshipInvalidity(true);
+    }
+  }, [data.township]);
+
+  // Check that range ends in a valid character
+  useEffect(() => {
+    if (data.range[3] === 'E' || data.range[3] === 'W') {
+      setRangeInvalidity(false);
+    } else {
+      setRangeInvalidity(true);
+    }
+  }, [data.range]);
 
   return (
     <VStack>
@@ -28,7 +45,7 @@ function SectionName({ data, setData }) {
             type="text"
             value={data.section}
             width="75px"
-            onChange={(e) => setData({ ...data, section: (e.target.value).toUpperCase() })}
+            onChange={(e) => setData({ ...data, section: (e.target.value).toUpperCase(), sectionName: sectionNameInput })}
           />
           <FormErrorMessage>{sectionError}</FormErrorMessage>
           <FormHelperText fontSize={10}>{sectionHelper}</FormHelperText>
@@ -36,13 +53,13 @@ function SectionName({ data, setData }) {
         <FormControl isRequired>
           <FormLabel>Township</FormLabel>
           <Input
-            isInvalid={() => isTownshipInvalid(data.township)}
+            isInvalid={townshipInvalidity}
             maxLength={4}
             minLength={4}
             type="text"
             value={data.township}
             width="85px"
-            onChange={(e) => setData({ ...data, township: (e.target.value).toUpperCase() })}
+            onChange={(e) => setData({ ...data, township: (e.target.value).toUpperCase(), sectionName: sectionNameInput })}
           />
           <FormErrorMessage>{townshipError}</FormErrorMessage>
           <FormHelperText fontSize={10}>{townshipHelper}</FormHelperText>
@@ -50,13 +67,13 @@ function SectionName({ data, setData }) {
         <FormControl isRequired>
           <FormLabel>Range</FormLabel>
           <Input
-            isInvalid={() => isRangeInvalid(data.range)}
+            isInvalid={rangeInvalidity}
             maxLength={4}
             minLength={4}
             type="text"
             value={data.range}
             width="85px"
-            onChange={(e) => setData({ ...data, range: (e.target.value).toUpperCase() })}
+            onChange={(e) => setData({ ...data, range: (e.target.value).toUpperCase(), sectionName: sectionNameInput })}
           />
           <FormErrorMessage>{rangeError}</FormErrorMessage>
           <FormHelperText fontSize={10}>{rangeHelper}</FormHelperText>
